ceb5f9cc51aab5a8e972595c75f15829829c44d7
Linggar Bhakti Pratama <linggarbhakti450@gmail.com>
2024-05-17 22:51:52 +0700

    Complete update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,20 +3,22 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
+        ParkingCalculation(24);
         ParkingCalculation(25);
         ParkingCalculation(26);
         ParkingCalculation(9);
         ParkingCalculation(8);
+        ParkingCalculation(5);
     }
 
     public static void ParkingCalculation(int hours){
         float parkingFees;
-        if (hours <24){
+        if (hours < 24){
             parkingFees = FiveHours(hours);
-            System.out.println("$"+parkingFees);
+            System.out.println(hours + " hours = $" + parkingFees);
         }else{
             parkingFees = TwentyFourHours(hours);
-            System.out.println("$"+parkingFees);
+            System.out.println(hours + " hours = $" + parkingFees);
         }
     }
 
@@ -24,11 +26,11 @@ public class Main {
         float parkingFees;
         int hours;
 
-        if (hoursAmount>5){
-            hours = hoursAmount-5;
-            parkingFees = (float) (1+(hours *0.5));
+        if (hoursAmount > 5){
+            hours = hoursAmount - 5;
+            parkingFees = (float) (1 + (hours * 0.5));
         }else{
-            parkingFees = (float) 1;
+            parkingFees = 1;
         }
         return parkingFees;
     }
@@ -37,19 +39,21 @@ public class Main {
         float parkingFees;
         int hours;
 
-        if (hoursAmount>24){
-            hours = hoursAmount-24;
-            parkingFees = (float) (15+(hours *0.5));
+        if (hoursAmount > 24){
+            hours = hoursAmount - 24;
+            parkingFees = (float) (15 + (hours * 0.5));
         }else{
-            parkingFees = (float) 15;
+            parkingFees = 15;
         }
         return parkingFees;
     }
 }
 
 /*
-$15.5
-$16.0
-$3.0
-$2.5
+24 hours = $15.0
+25 hours = $15.5
+26 hours = $16.0
+9 hours = $3.0
+8 hours = $2.5
+5 hours = $1.0
 */
