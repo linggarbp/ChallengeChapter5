7f60e6d782b404ecca8b0df95d8ff25418a306e4
Linggar Bhakti Pratama <linggarbhakti450@gmail.com>
2024-05-17 22:43:10 +0700

    Complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,55 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        ParkingCalculation(25);
+        ParkingCalculation(26);
+        ParkingCalculation(9);
+        ParkingCalculation(8);
+    }
+
+    public static void ParkingCalculation(int hours){
+        float parkingFees;
+        if (hours <24){
+            parkingFees = FiveHours(hours);
+            System.out.println("$"+parkingFees);
+        }else{
+            parkingFees = TwentyFourHours(hours);
+            System.out.println("$"+parkingFees);
         }
     }
+
+    public static float FiveHours(int hoursAmount){
+        float parkingFees;
+        int hours;
+
+        if (hoursAmount>5){
+            hours = hoursAmount-5;
+            parkingFees = (float) (1+(hours *0.5));
+        }else{
+            parkingFees = (float) 1;
+        }
+        return parkingFees;
+    }
+
+    public static float TwentyFourHours(int hoursAmount){
+        float parkingFees;
+        int hours;
+
+        if (hoursAmount>24){
+            hours = hoursAmount-24;
+            parkingFees = (float) (15+(hours *0.5));
+        }else{
+            parkingFees = (float) 15;
+        }
+        return parkingFees;
+    }
 }
+
+/*
+$15.5
+$16.0
+$3.0
+$2.5
+*/
